9acf73ec314c10f3eb72c9af76bf112bb565d206
ediso <ediso@DESKTOP-H2S89J8>
2016-09-05 07:57:57 -0500

    producto

---
+++ database/spProducto.sql
@@ -0,0 +1,40 @@
+use facturacion;
+delimiter //
+create procedure insertar_producto
+(in id varchar(40),
+ in nombre varchar(60),
+ in marca varchar(30),
+ in imagen varchar(40),
+ in stock int(10),
+ in precio_venta decimal(5,2),
+ in precio_inicial decimal(5,2)
+ )
+begin
+	insert into producto values (null, nombre, marca, imagen, stock, precio_venta, precio_inicial);
+end
+//
+
+delimiter //
+create procedure eliminar_producto
+(in idP varchar(40)
+)
+begin
+	delete from producto where id = idP;
+end
+//
+
+
+delimiter //
+create procedure editar_producto
+(in pIdOriginal varchar(15),
+ in pId varchar(15),
+ in pNombre varchar(30)
+ )
+begin
+	update producto set
+		id = pId and
+        nombre = pNombre
+	where
+		id = pIdOriginal;
+end
+//
